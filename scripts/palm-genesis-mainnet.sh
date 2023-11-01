956db8310265578f540c8e26fcdc8d4f56ffbcad
Scott Fairclough <scott@hexosoft.co.uk>
2023-11-01 11:01:45 +0000

    support passing an api key to the docker build command for mainnet

--- scripts/palm-genesis-mainnet.sh
+++ scripts/palm-genesis-mainnet.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+apiKey="$1"
+
 rm -f genesis.json
 
-mv genesis-mainnet.json genesis.json
+url=$(jq -r '.params.engine.polybft.bridge.jsonRPCEndpoint' genesis-mainnet.json)
+url="$url$apiKey"
+
+cat genesis-mainnet.json | jq ".params.engine.polybft.bridge.jsonRPCEndpoint = \"$url\"" > genesis.json
