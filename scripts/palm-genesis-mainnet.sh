8257a89b0bcf8fb359cedca3b8520343107eaa9f
Scott Fairclough <scott@hexosoft.co.uk>
2023-11-28 11:05:34 +0000

    remove api keys from the docker build process

--- scripts/palm-genesis-mainnet.sh
+++ scripts/palm-genesis-mainnet.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-apiKey="$1"
-
 rm -f genesis.json
 
-url=$(jq -r '.params.engine.polybft.bridge.jsonRPCEndpoint' genesis-mainnet.json)
-url="$url$apiKey"
-
-cat genesis-mainnet.json | jq ".params.engine.polybft.bridge.jsonRPCEndpoint = \"$url\"" > genesis.json
+mv genesis-mainnet.json genesis.json
